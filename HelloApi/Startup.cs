67516b6bfbdc7971cdb0b3880a38447c0af0fa43
Laszlo Boke <xeondevelop@gmail.com>
2022-10-21 12:53:36 +0200

    publish profile

--- HelloApi/Startup.cs
+++ HelloApi/Startup.cs
@@ -26,7 +26,6 @@ namespace HelloApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -40,9 +39,10 @@ namespace HelloApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HelloApi v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HelloApi v1"));
 
             app.UseHttpsRedirection();
 
